2d995757c2e68f32cebeba7cc9bc073916a8e344
2smallears <2smallears@users.noreply.github.com>
2016-07-25 22:40:14 +0800

    move buildReceiptText function to receipt.buildText method

--- src/models/Receipt.js
+++ src/models/Receipt.js
@@ -1,24 +1,39 @@
 class Receipt {
 
-  constructor(receiptItems, total, savedTotal) {
+  constructor(receiptItems = []) {
+
+    let total = 0;
+    let savedTotal = 0;
+
+    for (const receiptItem of receiptItems) {
+      total += receiptItem.subtotal;
+      savedTotal += receiptItem.saved;
+    }
 
     this.receiptItems = receiptItems;
     this.total = total;
     this.savedTotal = savedTotal;
   }
 
-  static buildReceipt(receiptItems) {
+  buildText() {
+    const formatMoney = (money) => money.toFixed(2);
+    let receiptItemsText = this.receiptItems
+      .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;}).join('\n');
 
-  let total = 0;
-  let savedTotal = 0;
-
-  for (const receiptItem of receiptItems) {
-    total += receiptItem.subtotal;
-    savedTotal += receiptItem.saved;
+      return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(this.total)}(元)
+节省：${formatMoney(this.savedTotal)}(元)
+**********************`;
   }
 
-  return new Receipt(receiptItems, total, savedTotal);
 }
-}
+
 
 module.exports = Receipt;
